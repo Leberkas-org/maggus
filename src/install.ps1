c39c99ee9f093bba1af43c48e4a1da8d4e98d251
Christian Dirnhofer <christian@dirnhofer.net>
2026-04-13 22:34:46 +0200

    maggus: recover uncommitted changes from bugfix/maggus-bug-049/task-001

--- src/install.ps1
+++ src/install.ps1
@@ -5,6 +5,10 @@ Push-Location $dir
 try {
     Write-Host -ForegroundColor DarkRed "Stopping all maggus instances..."
     maggus stop --all
+    Stop-Process -Name maggus -Force -ErrorAction SilentlyContinue
+    while (Get-Process -Name maggus -ErrorAction SilentlyContinue) {
+        Start-Sleep -Milliseconds 200
+    }
     $buildTime = Get-Date -Format "HHmmss"
     Write-Host -ForegroundColor DarkCyan "Compile to C:\bin\maggus.exe (dev-$buildTime)"
 
